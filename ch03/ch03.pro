f52c52fcba3a9687c3aaacd96c0166b934a62b1d
mingfeii <mingfeii@outlook.com>
2017-10-30 20:39:01 +0800

    add some files

--- ch03/ch03.pro
+++ ch03/ch03.pro
@@ -6,4 +6,7 @@ CONFIG -= qt
 SOURCES += main.cpp \
     ex3_24.cpp \
     ex3_25.cpp \
-    ex3_34.cpp
+    ex3_34.cpp \
+    ex3_43.cpp \
+    ex3_44.cpp \
+    ex3_45.cpp
--- ch03/main.cpp
+++ ch03/main.cpp
@@ -2,7 +2,7 @@
 #include <iterator>
 #include <vector>
 #include <cstring>
-//#include "ex3_34.cpp"
+#include "ex3_43.cpp"
 
 using namespace std;
 
@@ -55,10 +55,8 @@ void ex3_39()
         cout << "same string" << endl;
     else if (s1 > s2)
             cout << "s1 > s2" << endl;
-    else
-            cout << "s1 < s2" << endl;
-
-        cout << "=========用c风格字符串比较:" << endl;
+    else cout << "s1 < s2" << endl;
+    cout << "=========用c风格字符串比较:" << endl;
     const char *cs1 = s1.c_str();
     const char *cs2 = s2.c_str();
     auto res = strcmp(cs1,cs2);
@@ -93,13 +91,74 @@ void ex3_41()
         cout << i << " ";
     cout << endl;
 }
+void ex3_42()
+{
+    //vector对象初始化一个数组
+    vector<int> v = {1,2,3,4,5,6,7,8,9};
+    int arr[v.size()];
+    for (decltype(v.size()) i = 0; i < v.size(); ++i)
+       arr[i] = v[i];
+    for (auto i: arr)
+        cout << i << " ";
+    cout << endl;
+
+
+}
+//二维数组 普通版本
+void arr2arr0()
+{
+    int arr[3][4] = {{1,1,1,1},{2,2,2,2},{3,3,3,3}};
+    for (auto p = arr; p != arr + 3; ++p)
+    {    for (auto q = *p; q != *p + 4; ++q)
+            cout << *q << ' ';
+        cout << endl;
+    }
+}
+//二维数组 范围for语句版本
+void arr2arr1()
+{
+    int arr[3][4] = {{1,1,1,1},{2,2,2,2},{3,3,3,3}};
+    for (const auto &row: arr) //最外层必须用引用，防止自动转化为指针
+    {
+        for (auto col : row)
+            cout << col << ' ';
+        cout << endl;
+    }
+
+}
+//二维数组 迭代器版本
+void arr2arr2()
+{
+    int arr[3][4] = {{1,1,1,1},{2,2,2,2},{3,3,3,3}};
+    for (auto p = begin(arr); p != end(arr); ++p)
+    {
+        for (auto q = begin(*p); q != end(*p); ++q)
+            cout << *q << ' ';
+        cout << endl;
+    }
+}
+
+//二维数组 类型别名版本
+void arr2arr3()
+{
+    int arr[3][4] = {{1,1,1,1},{2,2,2,2},{3,3,3,3}};
+    using int_array = int[4]; //typedef int int_array[4] typedef bool(*FuncP)(const string &) ->函数指针类型
+    for (int_array *p = arr; p != arr + 3; ++p)
+    {    for (int *q = *p; q != *p + 4; ++q)
+            cout << *q << ' ';
+        cout << endl;
+    }
+}
+
 int main()
 {
-    ex3_41();
+    //arr2arr3();
     // ex3_39();
     //ex3_35();
     //ex3_34();
     //ex3_25();
     //cout << "Hello World!" << endl;
-    return 0;
+   ex3_43();
+
+   return 0;
 }
