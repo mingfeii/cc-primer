89fbf523636f2b8af111494a7ef19ae0927144ab
mingfeii <mingfeii@outlook.com>
2017-11-03 20:51:30 +0800

    nothing 2 du

--- ch08/ex8_5.cpp
+++ ch08/ex8_5.cpp
@@ -2,30 +2,24 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
 using namespace std;
+
 void ReadFileToVec(const string &filename,vector<string> &vec)
 {
     ifstream ifs(filename);
     if (ifs)
     {
         string buf;
-        while (getline(ifs,buf))
+        while (ifs >> buf)
             vec.push_back(buf);
     }
 }
 
-void ReadFileTest()
-{
-    vector<string> vec;
-    ReadFileToVec("./test.txt",vec);
-    for (const auto &str : vec)
-        cout << str << endl;
-}
-
 int main()
 {
-    ReadFileTest();
-
+    vector<string> vec;
+    ReadFileToVec("./test.txt", vec);
+    for (const auto &str : vec)
+        cout << str << endl;
     return 0;
 }
